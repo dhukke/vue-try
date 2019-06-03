b56a647b9925f7f5ba2e2969391185a685549716
dhukke <dhukke@gmail.com>
2019-06-03 18:31:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Test',
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+#body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+   <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{todo.title}}</h3>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  props: ['todos']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
